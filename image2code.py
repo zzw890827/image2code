05b27249fb6714458ea3b021f6bc6cc1b8499502
Lee Wenqian <zzw890827@gmail.com>
2018-02-14 17:31:08 +0900

    Add code list; Create main.py file.

---
+++ image2code.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+
+
+class Image2Code:
+    def __init__(self):
+        # pixel code list
+        self.pixel_list = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
---
+++ main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from image2code import Image2Code
+
+if __name__ == '__main__':
+    image_encode = Image2Code()
+    print(image_encode.pixel_list)
