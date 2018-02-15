0ca4ff558d04538ac36dae1af8b37f424fc78050
Lee Wenqian <zzw890827@gmail.com>
2018-02-15 09:08:15 +0900

    启用中文注释，定义字符映射方法。

--- image2code.py
+++ image2code.py
@@ -5,5 +5,12 @@ from PIL import Image
 
 class Image2Code:
     def __init__(self):
-        # pixel code list
+        # 字符集列表
         self.pixel_list = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+
+    '''
+    字符映射方法
+    
+    '''
+    def color_map(self, r, g, b, alpha=256):
+        print('即将到来')
